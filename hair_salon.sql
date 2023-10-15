14ca23080931031d99ad1b6c979187cfc87e73ab
Helen Vu <blablabla@gmail.com>
2023-10-15 23:44:26 +0100

    populate treatments table in DB

--- hair_salon.sql
+++ hair_salon.sql
@@ -20,21 +20,20 @@ CREATE TABLE stylists (
 	mobile VARCHAR(15)
 );
 
-CREATE TABLE treatments (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(150) NOT NULL,
-    type VARCHAR(50),
-	description VARCHAR(400),
-	price DECIMAL(10,2),
-	duration TIME
-);
-
 CREATE TABLE salon_info (
     id INT AUTO_INCREMENT PRIMARY KEY,
     salon_name VARCHAR(100),
     telephone INT,
     email VARCHAR(100),
     address VARCHAR(200)
+);
+
+CREATE TABLE treatments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(150) NOT NULL,
+	description VARCHAR(400),
+	price DECIMAL(10,2),
+	duration TIME
 );
 
 CREATE TABLE opening_hours (
@@ -65,3 +64,19 @@ VALUES
 INSERT INTO stylists (first_name, last_name, mobile)
 VALUES
 	('Sophie', 'Jackson', '0798865432');
+
+INSERT INTO treatments (name, description, price, duration)
+VALUES
+	('Blowdry', 'Blowdry styling only', 30.00, '00:25:00'),
+	('Dry Cut', 'Dry cut on clean hair', 50.00, '00:30:00'),
+	('Wash & Blowdry', 'Hair wash, and blowdry styling', 40.00, '00:30:00'),
+    ('Cut & Blowdry', 'Dry cut and blowdry styling', 60.00, '00:45:00'),
+    ('Wash, Cut & Blowdry', 'Hair wash, scalp massage, cut and blowdry/heat styling', 70.00, '01:00:00'),
+    ('Colour - Full head', 'Hair colour dying - whole head', 150.00, '02:00:00'),
+	('Colour - Roots', 'Top of of colour on roots', 75.00, '01:15:00'),
+    ('Highlights', 'Whole head of highlights', 120.00, '01:45:00'),
+    ('Perm - Wavy', 'Dry cut on short - medium length hair', 200.00, '02:00:00'),
+    ('Perm - Straight', 'Dry cut on short - medium length hair', 200.00, '02:00:00');
+    
+    
+    
